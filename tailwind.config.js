94a4b8467fe5902ab389a6e0ba995805cb6ba49b
0wlboy <35744878+0wlboy@users.noreply.github.com>
2024-10-18 09:59:19 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   theme: {
     colors:{},
     extend: {},
+    screens: {
+      'tablet': '601px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1025px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+
   },
   plugins: [
     require('@tailwindcss/forms')
